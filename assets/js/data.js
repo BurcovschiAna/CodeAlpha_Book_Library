70eec7263929fe489a1ab37fa1beda4790dcfc52
Ana Burcovschi <ana@burcovschi.com>
2024-12-22 16:28:39 +0200

    save book

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,114 +1,130 @@
 let books = [
     {
         bookTitle: "A to Z Crochet",
-        bookAuthor: "",
+        bookAuthor: "unanimous",
         bookImg: "./assets/img/A to Z Crochet.jpg",
         bookPath: "#",
-        category: "Hobbies & Crafts"
+        category: "Hobbies and Crafts",
+        bookIndex: "0"
     },
     {
         bookTitle: "Anna Karenina",
         bookAuthor: "Leo TolstoY",
         bookImg: "./assets/img/Ana Karenina.webp",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "1"
     },
     {
         bookTitle: "Big Magic",
         bookAuthor: "Elizabeth Gilbert",
         bookImg: "./assets/img/Big Magic.webp",
         bookPath: "#",
-        category: "Personal Growth"
+        category: "Personal Growth",
+        bookIndex: "2"
     },
     {
         bookTitle: "Cell Biology",
         bookAuthor: "George Plopper and Diana Bebek Ivankovic",
         bookImg: "./assets/img/Cell Biology.jpg",
         bookPath: "#",
-        category: "Academic"
+        category: "Academic",
+        bookIndex: "3"
     },
     {
         bookTitle: "Chrochet Pattern Book",
         bookAuthor: "Nancy Gordon",
         bookImg: "./assets/img/Chroche Pattern Book.jpg",
         bookPath: "#",
-        category: "Hobbies & Crafts"
+        category: "Hobbies and Crafts",
+        bookIndex: "4"
     },
     {
         bookTitle: "Failig Forward",
         bookAuthor: "John C. Maxwell",
         bookImg: "./assets/img/Failing Forward.webp",
         bookPath: "#",
-        category: "Personal Growth"
+        category: "Personal Growth",
+        bookIndex: "5"
     },
     {
         bookTitle: "Harry Potter and the Deathly Hallows",
-        bookAuthor: "c",
+        bookAuthor: "J.K Rowling",
         bookImg: "./assets/img/Harry Potter and The deathly Hallows.jpg",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "6"
     },
     {
         bookTitle: "Harry Potter and the Prisoner of Askaban",
-        bookAuthor: "Harry Potter Prisoner of askaban",
+        bookAuthor: "J.K Rowling",
         bookImg: "./assets/img/Harry Potter Prisoner of askaban.jpg",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "7"
     },
     {
         bookTitle: "History of the World",
         bookAuthor: "J.M. Roberts and O.A Westad",
         bookImg: "./assets/img/History of the World.jpg",
         bookPath: "#",
-        category: "Academic"
+        category: "Academic",
+        bookIndex: "8"
     },
     {
         bookTitle: "Hobbit",
         bookAuthor: "J.R.R Tolkien",
         bookImg: "./assets/img/Hobbit.jpg",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "9"
     },
     {
         bookTitle: "The Chronicles of Narnia",
         bookAuthor: "C.S Lewis",
         bookImg: "./assets/img/Narnia.jpg",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "10"
     },
     {
         bookTitle: "The complete Book of Origami",
-        bookAuthor: "",
+        bookAuthor: "unanimous",
         bookImg: "./assets/img/Origami.jpg",
         bookPath: "#",
-        category: "Hobbies & Crafts"
+        category: "Hobbies and Crafts",
+        bookIndex: "11"
     },
     {
         bookTitle: "The art of Paper Craft",
         bookAuthor: "Helen Hiebert",
         bookImg: "./assets/img/Paper Caft.jpg",
         bookPath: "#",
-        category: "Hobbies & Crafts"
+        category: "Hobbies and Crafts",
+        bookIndex: "12"
     },
     {
         bookTitle: "The Book of Biology",
         bookAuthor: "Michael C. Gerald and Gloria Gerald",
         bookImg: "./assets/img/The Book of Biology.jpg",
         bookPath: "#",
-        category: "Academic"
+        category: "Academic",
+        bookIndex: "13"
     },
     {
         bookTitle: "The history of the Ancient World",
         bookAuthor: "Susan Wise Bauer",
         bookImg: "./assets/img/The history of ancient World.jpg",
         bookPath: "#",
-        category: "Academic"
+        category: "Academic",
+        bookIndex: "14"
     },
     {
         bookTitle: "The Idiot",
         bookAuthor: "Fyodor Dostoevsky",
         bookImg: "./assets/img/The idiot.jpeg",
         bookPath: "#",
-        category: "Culture"
+        category: "Culture",
+        bookIndex: "15"
     },
 ]
--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,31 +1,130 @@
 const allBooks = document.querySelector("#all-books");
 const category = document.querySelectorAll(".book-category");
+const allCategories = document.querySelector("#all-categories");
+const searchInput = document.querySelector("#search-input");
+const searchBtn = document.querySelector("#search-btn");
+const modalMessage = document.querySelector("#modal-message");
+
+var modal = new bootstrap.Modal(document.querySelector('#modal-error'));
+let cards = [];
+let likedBookIndices = [];
+
+category.forEach(category => {
+    category.addEventListener("click", displayCategory);
+})
+allCategories.addEventListener("click", function() {
+    displayBooks(books); // Call displayBooks when allCategories is clicked
+});
+searchInput.addEventListener("keydown", searchByEnter);
+searchBtn.addEventListener("click", search);
 
 
-function displayBooks() {
-    for (let i = 0; i < books.length; i++) {
+
+
+function displayBooks(source) {
+    allBooks.innerHTML = '';
+    cards = [];
+    source.forEach(book => {
         let card = document.createElement("div");
-        card.classList.add("col-xxl-2", "col-lg-3", "col-md-4", "col-sm-6");
-        
-        // Correctly set the innerHTML property
+        card.classList.add("col-xxl-2", "col-lg-3", "col-md-4", "col-sm-6", "mx-auto");
+        card.setAttribute("data-type", `${book.category}`);
+        card.setAttribute("data-index", `${book.bookIndex}`);
         card.innerHTML = `
         <div class="card bg-light-color border-0">
-            <img src="${books[i].bookImg}" class="card-img-top shadow-sm">
+            <div class="img-card">
+                <img src="${book.bookImg}">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">
-                    ${books[i].bookTitle}
+                    ${book.bookTitle}
                 </h5>
-                <p>${books[i].bookAuthor}</p>
+                <p>${book.bookAuthor}</p>
                 <div class="d-flex justify-content-between">
                     <a href="#" class="btn btn-darker-green"><i class="bi bi-arrow-down-circle"></i></a>
-                    <div><i class="bi bi-heart fs-4"></i></div>
+                    <div class="liked-book"><i class="bi bi-heart fs-4"></i></div>
                 </div>
             </div>
-        </div>`;
+          </div>`;
         
         allBooks.appendChild(card);
+        cards.push(card);
+    }); 
+    const likedBooks = document.querySelectorAll(".liked-book");
+    likedBooks.forEach(book =>{
+    book.addEventListener("click", like)
+}) 
+initializeLikedBooks();
+}
+displayBooks(books);
+function displayCategory(event){
+    
+    for(let i = 0; i < cards.length; i++){
+        cards[i].classList.add("d-none")
+    }
+    const selectedCategory = event.target.innerHTML.trim();
+
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].getAttribute("data-type") === selectedCategory) {
+            cards[i].classList.remove("d-none");
+        }
     }
 }
 
-displayBooks();
-displayBooks();
+
+function search() {
+    let value = searchInput.value.trim().toLowerCase();
+    if (value) {
+        let filteredBooks = books.filter(function(book) {
+            return book.bookTitle.toLowerCase().indexOf(value) === 0 || 
+                   book.bookAuthor.toLowerCase().indexOf(value) === 0;
+        });
+        if (filteredBooks.length > 0) {
+            displayBooks(filteredBooks); 
+        } else {
+            modalMessage.innerHTML = `Sorry we do not have a books with name or author "${value}."`
+            modal.show(); 
+        }
+    } else {
+        modalMessage.innerHTML = "You didn't enter a book name or author."
+        modal.show(); 
+    }
+}
+
+function searchByEnter(event) {
+    if (event.key === 'Enter') {
+        search(); 
+    }
+}
+
+function initializeLikedBooks() {
+    const savedLikedBooks = localStorage.getItem("likedBooks");
+    if (savedLikedBooks) {
+        likedBookIndices = JSON.parse(savedLikedBooks);
+        likedBookIndices.forEach(index => {
+            const card = document.querySelector(`[data-index="${index}"]`);
+            if (card) {
+                const heartIcon = card.querySelector(".liked-book i");
+                heartIcon.classList.add("bi-heart-fill", "text-danger");
+                heartIcon.classList.remove("bi-heart");
+            }
+        });
+    }
+}
+
+
+function like() {
+    const heartIcon = this.firstElementChild; 
+    heartIcon.classList.toggle("bi-heart");
+    heartIcon.classList.toggle("bi-heart-fill");
+    heartIcon.classList.toggle("text-danger");
+    const card = this.closest(".card");    
+    const bookIndex = card.parentElement.getAttribute("data-index"); 
+    if (heartIcon.classList.contains("bi-heart-fill")) {
+        likedBookIndices.push(bookIndex); 
+    } else {
+        likedBookIndices = likedBookIndices.filter(index => index !== bookIndex);  
+    }
+    localStorage.setItem("likedBooks", JSON.stringify(likedBookIndices)); 
+    console.log(likedBookIndices);
+}
+initializeLikedBooks();
